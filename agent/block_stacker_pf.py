8f6c2c9aa8551d8abe4fba64100be63b5127b187
Binit Shah <binitpshah@gmail.com>
2020-03-11 21:03:07 -0400

    updated to match ammars planning library

--- agent/block_stacker_pf.py
+++ agent/block_stacker_pf.py
@@ -13,12 +13,14 @@ import localization
 class BlockStacker:
     """Brain of the block stacker robot"""
 
-    def __init__(self, starting_block_config, starting_pose=(0, 0, 0), system_type="sim", sim_config=None, pose_confidence_threshold=0.95):
+    def __init__(self, starting_block_goals, starting_pose=(0, 0, 0), system_type="sim",
+                 sim_config=None, pose_confidence_threshold=0.95):
         interface.set_system(system_type, sim_config=sim_config)
 
         self.curr_pose = starting_pose                                      # (x_m, y_m, theta_rad)
-        self.remaining_goals = planning.order_blocks(starting_block_config) # [block: (x_m, y_m, color)]
-        self.goal = self.remaining_goals.pop(0)                             # (x_m, y_m, color)
+        self.remaining_goals = starting_block_goals                         # [block: (bin_num, offset_m)]
+        goal = self.remaining_goals.pop(0)                                  # (bin_num, offset_m)
+        planning.queue_bin(self.curr_pose, goal[0], goal[1])
         self.time_left_s = 180.0                                            # 3 minutes
         self.prev_time_s = interface.get_time()
 
@@ -29,15 +31,16 @@ class BlockStacker:
             self.prev_time_s = interface.get_time()
             return
 
-        if planning.reached(self.curr_pose, self.goal):
-            self.goal = self.remaining_goals.pop(0)
+        if planning.wp_done():
+            goal = self.remaining_goals.pop(0)
+            planning.queue_bin(self.curr_pose, goal[0], goal[1])
 
         self.curr_pose, confidence = localization.compute_mean_pose()
         if confidence < self.POSE_CONFIDENCE_THRES:
             localization.measurement_update(vision.find_landmarks(vision.ignore_outside(interface.read_image())))
             self.curr_pose, confidence = localization.compute_mean_pose()
 
-        omegas = planning.compute_wheel_velocities(self.curr_pose, self.goal)
+        omegas = planning.compute_wheel_velocities(self.curr_pose)
         interface.command_wheel_velocities(omegas)
 
         now = interface.get_time()
